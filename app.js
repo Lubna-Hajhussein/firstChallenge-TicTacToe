6dccd7f2c79d3f0b4ca506b2b02d82a652bc6430
lubna <lubnahaj45@gmail.com>
2020-06-18 13:14:02 +0300

    done with horiz

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var allRows =document.querySelectorAll('tr')
 var isXorY = false
-allRows.forEach(function(row){
+allRows.forEach(function(row,i){
    row.querySelectorAll('td').forEach(function(square){
        square.addEventListener('click',function(e){
            //check if the square is empty
@@ -14,11 +14,57 @@ allRows.forEach(function(row){
             square.textContent = 'O'
             isXorY = 'O'
         }
+        checkHorizontallyForOneRow(i)
        })
+   
    })
+  
 })
 //check horizontally 
-
+//1-check for one row
+//console.log(allRows[0].querySelectorAll('td'))
+function checkHorizontallyForOneRow(rowIndex){
+    var isAllX =0
+    var isAllO =0
+   allRows[rowIndex].querySelectorAll('td').forEach(function(square){
+      if(square.textContent==='X'){
+          isAllX++
+      }
+      if(square.textContent==='O'){
+          isAllO++
+      }
+   })
+   if(isAllX===3||isAllO===3){
+       console.log('hi')
+    allRows[rowIndex].querySelectorAll('td').forEach(function(square){
+       square.style = "background-color: yellow;"
+        
+     })
+   }
+}
+function checkVerticallyForOneColumn(squareIndex){
+    //each column is the first three elements in each row
+   var isAllX=0
+    var isAllO=0 
+  allRows.forEach(function(row){
+      row.querySelectorAll('td').forEach(function(square,i){
+          if(i===squareIndex){
+              if(square.textContent==='X'){
+              isAllX++}
+              if(square.textContent==='O'){
+                  isAllO++
+              }
+          }
+      })
+  })
+  if(isAllX===3||isAllO===3){
+    console.log('hi')
+ allRows[rowIndex].querySelectorAll('td').forEach(function(square){
+    square.style = "background-color: yellow;"
+     
+  })
+}
+}
 // Implement the rules of Tic Tac Toe
 // Before placing an X or O, ensure the clicked board square is empty. If the position is
 //  already occupied, don't place an X or O and do not move on to the next player's turn.
